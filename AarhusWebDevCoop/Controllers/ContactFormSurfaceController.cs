e497db3d3de64171cca8a17ea3494f827fd25b00
Guillem Pujol Griñó <guille8.bcn@gmail.com>
2019-10-02 11:34:52 +0200

    part 1

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return PartialView("_ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            MailMessage message = new MailMessage();
+            message.To.Add("username@eaaa.dk");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("username@gmail.com", "password");
+                // send mail 
+                smtp.Send(message);
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+        
+
+    }
+}

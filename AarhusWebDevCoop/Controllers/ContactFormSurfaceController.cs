908ee503385304318a3c3e3a043e8cd6fe7fb33b
Guillem Pujol Griñó <guille8.bcn@gmail.com>
2019-10-13 17:16:38 +0200

    comments

--- AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -13,14 +13,17 @@ using Umbraco.Core.Services;
 
 namespace AarhusWebDevCoop.Controllers
 {
+    //inherits from the Surface Controller
     public class ContactFormSurfaceController : SurfaceController
     {
         [HttpGet]
         public ActionResult Index()
         {
+            //display the partial view contact form
             return PartialView("_ContactForm", new ContactForm());
         }
 
+        // post method that creates a new message, adds it to my email and selects the subject, email and the message from the form.
         [HttpPost]
         public ActionResult HandleFormSubmit(ContactForm model)
         {
@@ -30,10 +33,11 @@ namespace AarhusWebDevCoop.Controllers
             message.From = new MailAddress(model.Email, model.Name);
             message.Body = model.Message;
 
+            // if we get errors we return the same page with the validation warnings and not an error page
             if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
 
             
-
+            // we use the smtp in order to send the message to my email
             using (SmtpClient smtp = new SmtpClient())
             {
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
@@ -41,24 +45,27 @@ namespace AarhusWebDevCoop.Controllers
                 smtp.EnableSsl = true;
                 smtp.Host = "smtp.gmail.com";
                 smtp.Port = 587;
-                smtp.Credentials = new System.Net.NetworkCredential("guille8.bcn@gmail.com", "Scolasticos8");
+                smtp.Credentials = new System.Net.NetworkCredential("guille8.bcn@gmail.com", "*********");
                 // send mail 
                 smtp.Send(message);
             }
 
+            // We use the temporal variable to use it in the view so we know if the message was sent or not
             TempData["success"] = true;
 
-            GuidUdi currentPageUdi = new GuidUdi(CurrentPage.ContentType.ItemType.ToString(), CurrentPage.Key);
+            // This code creates a comment type document behind the message document and save all the messages received
+                //gets the GuidUi of the current page
+                GuidUdi currentPageUdi = new GuidUdi(CurrentPage.ContentType.ItemType.ToString(), CurrentPage.Key);
 
+                // Creates the new content type
+                IContent msg = Services.ContentService.CreateContent(model.Subject, currentPageUdi, "message");
+                msg.SetValue("messageName", model.Name);
+                msg.SetValue("email", model.Email);
+                msg.SetValue("subject", model.Subject);
+                msg.SetValue("messageContent", model.Message);
+                msg.SetValue("umbracoNaviHide", true);
 
-            IContent msg = Services.ContentService.CreateContent(model.Subject, currentPageUdi, "message");
-            msg.SetValue("messageName", model.Name);
-            msg.SetValue("email", model.Email);
-            msg.SetValue("subject", model.Subject);
-            msg.SetValue("messageContent", model.Message);
-            msg.SetValue("umbracoNaviHide", true);
-
-            Services.ContentService.Save(msg);
+                Services.ContentService.Save(msg);
 
             return RedirectToCurrentUmbracoPage();
         }
